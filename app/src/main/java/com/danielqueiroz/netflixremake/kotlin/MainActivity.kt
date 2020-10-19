7226f4cb5f2b13ae5922921fd5dd4996bbfdc940
queirozdaniel <danielqueirozak77@gmail.com>
2020-10-19 01:09:24 -0300

    Refatorando código

--- app/src/main/java/com/danielqueiroz/netflixremake/kotlin/MainActivity.kt
+++ app/src/main/java/com/danielqueiroz/netflixremake/kotlin/MainActivity.kt
@@ -3,6 +3,7 @@ package com.danielqueiroz.netflixremake.kotlin
 import android.os.Bundle
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -39,68 +40,52 @@ class MainActivity : AppCompatActivity() {
         categoryTask.execute("https://tiagoaguiar.co/api/netflix/home")
     }
 
-    private inner class MainAdapter(
-            val categories: MutableList<Category>
-    ) : RecyclerView.Adapter<CategoryHolder>(){
+    private inner class MainAdapter(val categories: MutableList<Category>) : RecyclerView.Adapter<CategoryHolder>(){
 
-        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CategoryHolder {
-            return CategoryHolder(
-                    layoutInflater
-                            .inflate(R.layout.category_item, parent, false)
-            )
-        }
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CategoryHolder = CategoryHolder(
+                layoutInflater.inflate(R.layout.category_item, parent, false))
 
-        override fun onBindViewHolder(holder: CategoryHolder, position: Int) {
-            val category = categories[position]
-            holder.bind(category)
-        }
+        override fun onBindViewHolder(holder: CategoryHolder, position: Int) = holder.bind(categories[position])
 
         override fun getItemCount(): Int = categories.size
-
     }
 
-    private inner class MovieAdapter(
-            val movies: MutableList<Movie>
-    ) : RecyclerView.Adapter<MovieHolder>(){
-        val onClick: ((Int) -> Unit)? = { position ->
-            if (movies[position].id <= 3){
-                val intent = android.content.Intent(this@MainActivity, MovieActivity::class.java)
-                intent.putExtra("id", movies[position].id)
-                startActivity(intent)
-            }
-        }
+    private inner class MovieAdapter(val movies: MutableList<Movie>, private val listener: ((Movie) -> Unit)?) : RecyclerView.Adapter<MovieHolder>(){
 
-        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MovieHolder {
-            return MovieHolder(
-                    layoutInflater
-                            .inflate(R.layout.movie_item, parent, false),
-                    onClick
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MovieHolder =
+            MovieHolder(
+                    layoutInflater.inflate(R.layout.movie_item, parent, false),
+                    listener
             )
-        }
 
-        override fun onBindViewHolder(holder: MovieHolder, position: Int) {
-            val movie = movies[position]
-            holder.bind(movie)
-        }
+        override fun onBindViewHolder(holder: MovieHolder, position: Int) = holder.bind(movies[position])
 
         override fun getItemCount(): Int = movies.size
-
     }
 
     private inner class CategoryHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        fun bind(category: Category){
-            itemView.text_view_title.text = category.name
-            itemView.recycler_view_movie.adapter = MovieAdapter(category.movies)
-            itemView.recycler_view_movie.layoutManager = LinearLayoutManager(this@MainActivity, RecyclerView.HORIZONTAL, false)
+        fun bind(category: Category) = with(itemView){
+            text_view_title.text = category.name
+            recycler_view_movie.adapter = MovieAdapter(category.movies) { movie ->
+                if (movie.id > 3){
+                    Toast.makeText(this@MainActivity, "Não foi implementado", Toast.LENGTH_LONG).show()
+                } else {
+                    val intent = android.content.Intent(this@MainActivity, MovieActivity::class.java)
+                    intent.putExtra("id", movie.id)
+                    startActivity(intent)
+                }
+
+            }
+            recycler_view_movie.layoutManager = LinearLayoutManager(this@MainActivity, RecyclerView.HORIZONTAL, false)
         }
     }
 
-    private class MovieHolder(itemView: View, val onClick: ((Int) -> Unit)?) : RecyclerView.ViewHolder(itemView) {
-        fun bind(movie: Movie){
-            ImageDownloaderTask(itemView.image_view_cover)
+    private class MovieHolder(itemView: View, val onClick: ((Movie) -> Unit)?) : RecyclerView.ViewHolder(itemView) {
+        fun bind(movie: Movie) = with(itemView){
+            ImageDownloaderTask(image_view_cover)
                     .execute(movie.coverUrl)
-            itemView.image_view_cover.setOnClickListener {
-                onClick?.invoke(adapterPosition)
+            image_view_cover.setOnClickListener {
+                onClick?.invoke(movie)
             }
         }
     }
